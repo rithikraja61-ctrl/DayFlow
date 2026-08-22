18a6aba88318a7d8db99cbc2fb03558fbb119331
rithik <rithikraja61@gmail.com>
2026-08-22 09:50:48 +0530

    Backend Initialize with Spring boot

---
+++ Backend/DayFlow/src/main/java/com/DayFlow/DayFlowApplication.java
@@ -0,0 +1,13 @@
+package com.DayFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayFlowApplication.class, args);
+	}
+
+}
---
+++ Backend/DayFlow/src/test/java/com/DayFlow/DayFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DayFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
